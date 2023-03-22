72e9618e5c6794d75e27fa316501ccf102f83a8d
lonla <gatienjordanlonlaep@gmail.com>
2023-03-22 11:00:17 +0100

    format A4 alignment

--- src/main/java/com/petroleum/services/PrintHelper.java
+++ src/main/java/com/petroleum/services/PrintHelper.java
@@ -22,50 +22,50 @@ public class PrintHelper {
         PdfDocument pdf = new PdfDocument(new PdfWriter(output.getAbsolutePath(),new WriterProperties().addXmpMetadata()));
         pdf.setDefaultPageSize(PageSize.A4);
         PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA);
-        Document document = new Document(pdf).setFont(font).setFontSize(7);
+        Document document = new Document(pdf).setFont(font).setFontSize(8);
         float ratio = (float) 820.0 / 297;
         Paragraph paragraph = new Paragraph(String.format("%07d", invoice.getId()));
-        paragraph.setFixedPosition(76 * ratio, 258 * ratio, 100);
+        paragraph.setFixedPosition(76 * ratio, 267 * ratio, 100);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getClient());
-        paragraph.setFixedPosition(32 * ratio, 247 * ratio, 300);
+        paragraph.setFixedPosition(32 * ratio, 256 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getTransporter());
-        paragraph.setFixedPosition(177 * ratio, 247 * ratio, 200);
+        paragraph.setFixedPosition(137 * ratio, 256 * ratio, 200);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getProduct().getName());
-        paragraph.setFixedPosition(33 * ratio, 242 * ratio, 300);
+        paragraph.setFixedPosition(33 * ratio, 251 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getTruckNumber());
-        paragraph.setFixedPosition(174 * ratio, 242 * ratio, 300);
+        paragraph.setFixedPosition(174 * ratio, 251 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getVolume() + "");
-        paragraph.setFixedPosition(35 * ratio, 237 * ratio, 300);
+        paragraph.setFixedPosition(35 * ratio, 246 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getDriver());
-        paragraph.setFixedPosition(173 * ratio, 237 * ratio, 300);
+        paragraph.setFixedPosition(173 * ratio, 246 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(invoice.getLoadingDate()));
-        paragraph.setFixedPosition(53 * ratio, 232 * ratio, 300);
+        paragraph.setFixedPosition(53 * ratio, 241 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getDeliveryPlace());
-        paragraph.setFixedPosition(183 * ratio, 232 * ratio, 300);
+        paragraph.setFixedPosition(183 * ratio, 241 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(invoice.getLoadingDepot());
-        paragraph.setFixedPosition(56 * ratio, 227 * ratio, 300);
+        paragraph.setFixedPosition(56 * ratio, 236 * ratio, 300);
         document.add(paragraph);
 
         paragraph = new Paragraph(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(invoice.getReceiptDate()));
-        paragraph.setFixedPosition(184 * ratio, 227 * ratio, 300);
+        paragraph.setFixedPosition(184 * ratio, 236 * ratio, 300);
         document.add(paragraph);
 
         document.close();
