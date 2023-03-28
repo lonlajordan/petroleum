32af31d6cd8804878b95becbc9ddaf70c999d922
lonla <gatienjordanlonlaep@gmail.com>
2023-03-28 06:57:53 +0100

    map issue fixed

--- src/main/java/com/petroleum/mappers/ProductMapper.java
+++ src/main/java/com/petroleum/mappers/ProductMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface ProductMapper {
-    @Mapping(target = "volume", ignore = true)
     @Mapping(target = "invoices", ignore = true)
     @Mapping(target = "supplies", ignore = true)
     void update(@MappingTarget Product product, Product productDto);
