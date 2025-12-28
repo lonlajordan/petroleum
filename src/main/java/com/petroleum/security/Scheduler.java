8706ccbc8e65d416d5a5b3fe7e55986166cfe108
lonla <gatienjordanlonlaep@gmail.com>
2025-12-28 20:59:38 +0100

    Firebase integration

--- src/main/java/com/petroleum/security/Scheduler.java
+++ src/main/java/com/petroleum/security/Scheduler.java
@@ -1,5 +1,11 @@
 package com.petroleum.security;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.petroleum.enums.Role;
 import com.petroleum.enums.Status;
 import com.petroleum.models.Invoice;
@@ -16,12 +22,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.annotation.PostConstruct;
 import javax.mail.MessagingException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.YearMonth;
@@ -29,6 +38,7 @@ import java.time.format.TextStyle;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 
 @Slf4j
@@ -40,6 +50,22 @@ public class Scheduler {
     private final InvoiceRepository invoiceRepository;
     private final TransferRepository transferRepository;
     private final UserRepository userRepository;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            InputStream serviceAccount = new ClassPathResource("one-bills-firebase.json").getInputStream();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e) {
+            log.error("Error loading Firebase configurations", e);
+        }
+    }
 
     // Delete all logs every first of each month at midnight
     @Scheduled(cron = "@monthly", zone = "GMT+1")
@@ -66,6 +92,41 @@ public class Scheduler {
             }
         } catch (IOException | MessagingException e) {
             log.error("Error while generating monthly tax report", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 10 ? * MON", zone = "GMT+1")
+    public void sendReminder(){
+        sendNotification("ONE BILLS", new Random().nextBoolean() ? "Paye tes factuers ENEO facilement" : "Pay your ENEO bills easily");
+    }
+
+
+    @Scheduled(cron = "0 0 15 ? * SAT", zone = "GMT+1")
+    public void sendReminder2(){
+        sendNotification("ONE BILLS", new Random().nextBoolean() ? "Collecte tes points bonus, et enjoy avec tes proches" : "Collect your bonus points, and enjoy with your loved ones");
+    }
+
+
+
+    public void sendNotification(String title, String body) {
+        // Create the notification message
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        // Create the full FCM message with the target token
+        Message message = Message.builder()
+                .setTopic("all")
+                .setNotification(notification)
+                .build();
+
+        // Send the message
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+            log.info("Successfully sent message: {}", response);
+        } catch (Exception e) {
+            log.error("Failed to send message", e);
         }
     }
 
