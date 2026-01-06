c9abbd8b6b5616d8e6be1d032d33808a5befdffe
LONLA_G <gatien.lonla@cca-bank.com>
2026-01-06 08:56:16 +0100

    Removed firebase messaging

--- src/main/java/com/petroleum/security/Scheduler.java
+++ src/main/java/com/petroleum/security/Scheduler.java
@@ -1,11 +1,5 @@
 package com.petroleum.security;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
 import com.petroleum.enums.Role;
 import com.petroleum.enums.Status;
 import com.petroleum.models.Invoice;
@@ -22,14 +16,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.mail.MessagingException;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.YearMonth;
@@ -37,7 +29,6 @@ import java.time.format.TextStyle;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 
 
 @Slf4j
@@ -76,64 +67,6 @@ public class Scheduler {
         } catch (IOException | MessagingException e) {
             log.error("Error while generating monthly tax report", e);
         }
-    }
-
-    @Scheduled(cron = "0 20 11 ? * MON", zone = "GMT+1")
-    public void sendReminder(){
-        String message = new Random().nextBoolean() ? "Paye tes factuers ENEO facilement" : "Pay your ENEO bills easily";
-        boolean success = sendNotification("ONE BILLS", message);
-        if(!success){
-            sendNotification("ONE BILLS", message);
-        }
-    }
-
-
-    @Scheduled(cron = "0 0 15 ? * SAT", zone = "GMT+1")
-    public void sendReminder2(){
-        String message = new Random().nextBoolean() ? "Collecte tes points bonus, et enjoy avec tes proches" : "Collect your bonus points, and enjoy with your loved ones";
-        boolean success = sendNotification("ONE BILLS", message);
-        if(!success){
-            sendNotification("ONE BILLS", message);
-        }
-    }
-
-
-
-    public boolean sendNotification(String title, String body) {
-        try {
-            InputStream serviceAccount = new ClassPathResource("one-bills-firebase.json").getInputStream();
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (Exception e) {
-            log.error("Error loading Firebase configurations", e);
-            return false;
-        }
-        // Create the notification message
-        Notification notification = Notification.builder()
-                .setTitle(title)
-                .setBody(body)
-                .build();
-
-        // Create the full FCM message with the target token
-        Message message = Message.builder()
-                .setTopic("all")
-                .setNotification(notification)
-                .build();
-
-        // Send the message
-        try {
-            String response = FirebaseMessaging.getInstance().send(message);
-            log.info("Successfully sent message: {}", response);
-            return true;
-        } catch (Exception e) {
-            log.error("Failed to send message", e);
-        }
-        return false;
     }
 
 }
